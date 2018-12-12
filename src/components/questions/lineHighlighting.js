b136269ee6fe9e3f1259b8ed850b02833a9cfc44
aromakh <andriyromakh@gmail.com>
2018-12-12 17:24:54 +0200

    LineHighlighting

--- src/components/questions/lineHighlighting.js
+++ src/components/questions/lineHighlighting.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+import { Divider } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
 
 class CreateLineHighlightingForm extends Component {
   constructor(props) {
@@ -12,24 +13,67 @@ class CreateLineHighlightingForm extends Component {
         lines: [],
         text: []
       },
+      newLine: this.getNewLine(),
       name: 'LineHighlighting'
     };
   }
 
-  handleTextChange = event => {
-    this.setState({data: {text: event.target.value}});
+  getNewLine() { return {text: '', isCorrect: false}; }
+
+  handleCheckboxChange(e) { 
+    const {newLine} = this.state;
+    newLine.isCorrect = e.target.checked;
+    this.setState({newLine});
   }
 
-  addTextComponent() {
-    this.props.onComponentAdded(this.state);
+  onNewLineChange = event => {
+    var {newLine} = this.state;
+    newLine.text = event.target.value;
+    this.setState({newLine});
+  }
+
+  addLine() {
+    const {newLine} = this.state;
+    const {data} = this.state;
+
+    newLine.isCorrect == true ? data.lines.push(1) : data.lines.push(0);
+    data.text.push(newLine.text);
+
+    this.setState({newLine: this.getNewLine()});
+    this.setState({data});
+
+    this.sendToParent(data);
+    console.log(data);
+  }
+
+  sendToParent(data) {
+    const componentData = { data: data, name: this.state.name };
+    this.props.onQuestionDataChanged(componentData);
   }
 
   render() {
     return (
       <div>
-        <p>{this.state.name} Component</p>
+        <p>Lines:</p>
+        {
+          this.state.data.text.map((lineText, index) => {
+            const isCorrect = this.state.data.lines[index] == 1 ? 'true' : 'false';
+            return <p key={index}>Choice {index + 1}: {lineText} | isCorrect: {isCorrect}</p>;
+          })
+        }
+        <Divider />
+        <p>New Line</p>
+        <TextField
+          label="New choice" margin="normal" variant="outlined"
+          value={this.state.newLine.text} onChange={this.onNewLineChange}
+        />
+        <span>isCorrect:</span>
+        <Checkbox
+          color="primary"
+          checked={this.state.newLine.isCorrect} onChange={this.handleCheckboxChange.bind(this)}
+        />
         <br />
-        <Button type="button" variant="contained" onClick={this.addTextComponent.bind(this)} color="primary">Create</Button>
+        <Button type="button" variant="contained" onClick={this.addLine.bind(this)} color="primary">Add Line</Button>
       </div>
     );
   }
